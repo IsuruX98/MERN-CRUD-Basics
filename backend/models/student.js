2f0c276716f9c7fc81c446e57499760a85d5e2cd
IT21169380 <isurusanka98@gmail.com>
2022-12-17 01:27:30 +0530

    finished the first model

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,1 +1,25 @@
 const mongoose = require("mongoose");
+
+const schema = mongoose.Schema;
+
+//creating a new schema
+const studentSchema = new schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+});
+
+//creating the model
+const student = mongoose.model("student", studentSchema);
+
+//exporting the model
+module.exports = student;
